e81073dc187a2a552cfb83da4d7f4960788249d8
Javier Fernández Correa <javier.fernandez@externos.ifema.es>
2024-01-08 13:46:19 +0100

    Moving Platform Logic
    With Event Tick and CurrentLocation += VelocityVector*DeltaTime

---
+++ ObstacleAssault/Intermediate/Build/Win64/ObstacleAssaultEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.38.33133
+#include "E:/Unreal Projects/CursoUdemy/Unreal-Obstacle-project/ObstacleAssault-Repository/ObstacleAssault/Intermediate/Build/Win64/ObstacleAssaultEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
